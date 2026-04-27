9665d7303a2b774c70892134950159436f80708f
ardaselo47 <ardaselim191@gmail.com>
2026-04-27 18:58:51 +0300

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+// settings.gradle.kts
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "LootrunBot-Mod"
